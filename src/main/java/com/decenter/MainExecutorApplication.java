0662ef8599086ee9ebe7fcfcd4bf54c0e4adce17
pmaster <pmaster@163.com>
2020-10-10 16:27:59 +0800

    test

---
+++ src/main/java/com/decenter/MainExecutorApplication.java
@@ -0,0 +1,15 @@
+package com.decenter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MainExecutorApplication {
+
+
+    public static void main(String[] args) {
+        SpringApplication.run(MainExecutorApplication.class, args);
+    }
+
+
+}
